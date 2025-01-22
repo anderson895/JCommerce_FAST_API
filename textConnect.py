187239c86e6daa288721c0ac287d0560e98c8193
JARP <105678913+anderson895@users.noreply.github.com>
2025-01-22 13:44:01 +0800

    Update textConnect.py

--- textConnect.py
+++ textConnect.py
@@ -2,7 +2,6 @@ import psycopg2
 
 # Replace with your actual connection details
 DATABASE_URL = "postgresql://postgres.dajcekyqugcgteookxfb:DNWhZ7UsPWnMxbN0@aws-0-ap-southeast-1.pooler.supabase.com:6543/postgres"
-
 try:
     # Attempt to connect
     conn = psycopg2.connect(DATABASE_URL)
